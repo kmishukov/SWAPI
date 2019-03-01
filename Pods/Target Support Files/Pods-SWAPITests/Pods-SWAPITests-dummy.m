a000df2110d11c93a12ce3e37cf3aea0448b60d1
Konstantin Mishukov <kkmishukov@gmail.com>
2019-03-01 22:16:46 +0300

    Removed PODs

--- Pods/Target Support Files/Pods-SWAPITests/Pods-SWAPITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWAPITests : NSObject
-@end
-@implementation PodsDummy_Pods_SWAPITests
-@end
--- Pods/Target Support Files/Pods-SWAPIUITests/Pods-SWAPIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SWAPIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SWAPIUITests
-@end
