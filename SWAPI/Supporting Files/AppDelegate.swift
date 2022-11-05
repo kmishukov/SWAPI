54368de7d61f5ebd936d29c19c1e2cb4438ec8a7
Konstantin Mishukov <kkmishukov@gmail.com>
2022-11-05 19:13:32 +0400

    Updated AppDelegate to SwiftLint

--- SWAPI/Supporting Files/AppDelegate.swift
+++ SWAPI/Supporting Files/AppDelegate.swift
@@ -14,7 +14,8 @@ import SnapKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         configureColorTheme()
         window?.rootViewController = LaunchViewController()
@@ -30,24 +31,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "SWAPI")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
-    
-    private func configureColorTheme(){
+
+    private func configureColorTheme() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor.swapiTitleBackground
         appearance.titleTextAttributes = [.foregroundColor: UIColor.swapiYellow]
-        
+
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().compactAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
         UINavigationBar.appearance().tintColor = UIColor.swapiYellow
     }
 }
-
